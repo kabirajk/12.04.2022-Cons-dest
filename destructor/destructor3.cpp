dd67aab8ff9e225e42d8f1ff656d66304e11eeb1
kabirajk <kabiraj.mc19@bitsathy.ac.in>
2022-04-12 20:16:53 +0530

    Destructor

---
+++ destructor/destructor3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+//session maintainance
+class auth
+{   public:
+    auth()
+    {
+        cout<<"logged in: started"<<endl;
+        //get auth()
+    }
+    //do sum work
+    ~auth()
+    {  
+        cout<<"logged out"<<endl;
+    }
+
+};
+class session
+{   public:
+    auth* ses=nullptr;
+    session()
+    {   if (ses!=nullptr)
+            delete ses;
+        ses=new auth;
+    }
+    void logout()
+    {
+        delete ses;
+        ses=nullptr;
+    }
+    ~session()
+    {
+        delete ses;
+        ses=nullptr;
+    }
+};
+
+int main()
+{
+    session s;
+    session s2;
+    return 0;
+}
